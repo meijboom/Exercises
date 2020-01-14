8ea6a1d00301653cd27755100597278981f8012a
Dennis <Dennis.meijboom@hotmail.com>
2020-01-14 11:37:19 +0100

    s

--- dates-and-text/dates-and-text/Program.cs
+++ dates-and-text/dates-and-text/Program.cs
@@ -8,33 +8,8 @@ namespace datesandtext
     public static void Main(string[] args)
     {
         var sentence = "this is going to be a really really really really really really really really really really really long text";
-        var summary = SymmerizeText(sentence, 25);
+        var summary = StringUtility.SummerizeText(sentence, 25);
             Console.WriteLine(summary);
-    }
-    public static string SymmerizeText(string text, int maxLength = 20)
-    {
-
-      if (text.Length < maxLength)
-      {
-        Console.WriteLine(text);
-      }
-
-      var words = text.Split(' ');
-      var totalCharacters = 0;
-      var summaryWords = new List<string>();
-
-      foreach (var word in words)
-      {
-        summaryWords.Add(word);
-
-        totalCharacters += word.Length + 1;
-        if (totalCharacters > maxLength)
-        {
-          break;
-        }
-      }
-      return String.Join(" ", summaryWords) + "....";
-
     }
   }
 }
--- dates-and-text/dates-and-text/SummerizeText.cs
+++ dates-and-text/dates-and-text/SummerizeText.cs
@@ -1,10 +1,57 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace datesandtext
 {
-    public class SummerizeText
+    public class SummerizeTextSt    public static string SymmerizeText(string text, int maxLength = 20)
     {
-        public SummerizeText()
+
+        if (text.Length < maxLength)
         {
+            Console.WriteLine(text);
+        }
+
+        var words = text.Split(' ');
+        var totalCharacters = 0;
+        var summaryWords = new List<string>();
+
+        foreach (var word in words)
+        {
+            summaryWords.Add(word);
+
+            totalCharacters += word.Length + 1;
+            if (totalCharacters > maxLength)
+            {
+                break;
+            }
+        }
+        return String.Join(" ", summaryWords) + "....";
+
+    }
+    {
+        public static string SummerizeText(string text, int maxLength = 20)
+        {
+
+            if (text.Length < maxLength)
+            {
+                Console.WriteLine(text);
+            }
+
+            var words = text.Split(' ');
+            var totalCharacters = 0;
+            var summaryWords = new List<string>();
+
+            foreach (var word in words)
+            {
+                summaryWords.Add(word);
+
+                totalCharacters += word.Length + 1;
+                if (totalCharacters > maxLength)
+                {
+                    break;
+                }
+            }
+            return String.Join(" ", summaryWords) + "....";
+
         }
     }
 }
